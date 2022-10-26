c45e3262f58f368d152c4d2eaee8d4567a41c99a
Eugene <Eugene.Nisnevich@Gmail.com>
2022-10-26 16:44:39 -0400

    final update

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-class Product {
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-    }
-}
-let productOne = new Product(Toothbrush, 5.99);
-let productTwo = new Product(Toothpaste, 8.99);
-let productThree = new Product(Floss, .99);
-let productFour = new Product(WaterPik, 53.99)
+let productsArray = []
+let pricesArray = []
 
-function displayproductList(){
-  let productList = document.querySelector(".product-list");
-  let productObjects = [productOne, productTwo, productThree, productFour]
-  let productItems = "";
-  for (Product of productObjects){
-    productItems += Product.name + "" + Product.price;
-  }
-  productList.innerHTML = productItems;
+function AddOne(){
+    productsArray.push("Crutches")
+    pricesArray.push(59.99)
 }
-displayproductList();
+
+function AddTwo(){
+    productsArray.push("Aircast Boot")
+    pricesArray.push(67.99)
+}
+
+function AddThree(){
+    productsArray.push("Knee Scooter")
+    pricesArray.push(125.00)
+}
+
+
+function CheckOut(){
+    let total = 0
+    pricesArray.forEach(x => {
+        total += x
+    })
+    
+    productsArray.forEach(x => {
+        let index = productsArray.indexOf(x)
+        let price = pricesArray[index]
+        let lineBreak = document.createElement("br")
+        let product = `${x}`
+        document.body.append(`${product}: ${price}`)
+        document.body.appendChild(lineBreak)
+    })
+
+    
+    let totalAppend = `Total: $${Number(total)}`
+    document.body.append(totalAppend)
+}
